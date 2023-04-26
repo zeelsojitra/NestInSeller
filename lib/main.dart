c41a456ed53a450ddd771d56062f6ede66abc5ed
iharshSojitra <harshsojitra2004@gmail.com>
2023-04-26 17:38:19 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         debugShowCheckedModeBanner: false,
-        home: Tab_Bar(),
+        home: Splash_Screen(),
       ),
     );
   }
